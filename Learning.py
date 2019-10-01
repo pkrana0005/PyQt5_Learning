e56ebbd5ad156d6b421a862a6447b287537b5b2a
Pawan Kumar <pkrana0005@gmail.com>
2019-10-01 15:09:31 +0530

    Simple window

--- Learning.py
+++ Learning.py
@@ -7,9 +7,7 @@ win = QMainWindow()
 win.setGeometry(200, 200, 400, 400)
 win.setWindowTitle("Mission Planer")
 
-label = QtWidgets.QLabel(win)
-label.setText("my first label")
-label.move(50, 50)
+
 
 win.show()
 sys.exit(app.exec_())
